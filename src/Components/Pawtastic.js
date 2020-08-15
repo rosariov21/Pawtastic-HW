3303287c3f5ba53289b77ff8f5ad008ae491ab15
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-15 15:36:47 -0400

    wireframe

---
+++ src/Components/Pawtastic.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react';
+
+export default class Pawtastic extends Component {
+
+    // place a contructor to this file 
+    constructor(props){
+        super(props){
+            this.state ={
+
+            }
+        }
+    }
+}
